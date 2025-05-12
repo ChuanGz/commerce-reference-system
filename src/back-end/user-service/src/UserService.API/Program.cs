e5368cbf76d3c28f30ea97f2e26ec3bc6d1a5366
Kendy <thanhnhan.lionus@gmail.com>
2025-05-12 21:42:22 +0700

    add mvc

--- src/back-end/user-service/src/UserService.API/Program.cs
+++ src/back-end/user-service/src/UserService.API/Program.cs
@@ -13,19 +13,28 @@ using UserService.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add MVC controllers
+builder.Services.AddControllers();
+
+// Swagger & healthchecks
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
+
 Console.WriteLine("UserService API is starting...");
 
+// EF Core DbContext
 builder.Services.AddDbContext<UserDbContext>(
     opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+// DI for repository + MediatR
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddMediatR(typeof(CreateUserCommandHandler));
+builder.Services.AddMediatR(typeof(CreateUserCommandHandler).Assembly);
 
 var app = builder.Build();
 
+// Enable Swagger in Development
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -33,6 +42,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Map healthchecks
 app.MapHealthChecks("/health");
+
+// Map controller routes
+app.MapControllers();
 
 app.Run();
