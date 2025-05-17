9167854c24ad3e7d70d0fec8a9b253affa01626f
Kendy <thanhnhan.lionus@gmail.com>
2025-05-17 15:17:34 +0700

    Add global usings for MediatR, domain entities, and repositories; refactor LoginCommandHandler to use BCrypt for password verification; update project references and add Microsoft.IdentityModel.Tokens packages.

--- src/back-end/identity-service/src/IdentityService.Application/GlobalUsings.cs
+++ src/back-end/identity-service/src/IdentityService.Application/GlobalUsings.cs
@@ -1,1 +1,9 @@
 global using MediatR;
+
+global using IdentityService.Domain.Entities;
+global using IdentityService.Domain.Repositories;
+
+global using System;
+global using System.Collections.Generic;
+global using System.Threading;
+global using System.Threading.Tasks;
