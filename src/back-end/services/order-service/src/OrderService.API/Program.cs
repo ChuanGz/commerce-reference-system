e4798d6fbae39738c9381d44dde9eaeccd2204e2
Kendy <thanhnhan.lionus@gmail.com>
2026-01-04 17:39:28 +0700

    Add Polly resilience policies for outbound HTTP

--- src/back-end/services/order-service/src/OrderService.API/Program.cs
+++ src/back-end/services/order-service/src/OrderService.API/Program.cs
@@ -1,5 +1,7 @@
 using OrderService;
 using Platform.Core.Extensions;
+using Polly;
+using Polly.Extensions.Http;
 
 var builder = WebApp.CreateWithDefaults<OrderServiceEntry>(args);
 
@@ -9,26 +11,40 @@ builder.Services.AddScoped<ICustomerServiceClient, CustomerServiceClient>();
 builder.Services.AddScoped<IProductServiceClient, ProductServiceClient>();
 builder.Services.AddScoped<IInventoryServiceClient, InventoryServiceClient>();
 
+var retryPolicy = HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .WaitAndRetryAsync(3, retry => TimeSpan.FromMilliseconds(200 * retry));
+
+var circuitBreakerPolicy = HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
+
 builder.Services.AddHttpClient<ICustomerServiceClient, CustomerServiceClient>(client => {
     client.BaseAddress = new Uri(
         builder.Configuration["Services:CustomerService:BaseUrl"] ?? "http://localhost:5050"
     );
     client.Timeout = TimeSpan.FromSeconds(30);
-});
+})
+    .AddPolicyHandler(retryPolicy)
+    .AddPolicyHandler(circuitBreakerPolicy);
 
 builder.Services.AddHttpClient<IProductServiceClient, ProductServiceClient>(client => {
     client.BaseAddress = new Uri(
         builder.Configuration["Services:ProductService:BaseUrl"] ?? "http://localhost:5060"
     );
     client.Timeout = TimeSpan.FromSeconds(30);
-});
+})
+    .AddPolicyHandler(retryPolicy)
+    .AddPolicyHandler(circuitBreakerPolicy);
 
 builder.Services.AddHttpClient<IInventoryServiceClient, InventoryServiceClient>(client => {
     client.BaseAddress = new Uri(
         builder.Configuration["Services:InventoryService:BaseUrl"] ?? "http://localhost:5040"
     );
     client.Timeout = TimeSpan.FromSeconds(30);
-});
+})
+    .AddPolicyHandler(retryPolicy)
+    .AddPolicyHandler(circuitBreakerPolicy);
 
 var app = builder.Build();
 
